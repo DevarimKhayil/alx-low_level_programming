c3098de14adc72d4950ab1257ea6d2d3847d13ae
devarim <wachirabriangeita@gmail.com>
2023-10-07 00:45:15 +0300

    damn

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - bears the main function
+  * Return: in this case 0
+  */
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
