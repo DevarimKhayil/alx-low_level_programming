628387fa62d884f609405bbed455901065c004de
devarim <wachirabriangeita@gmail.com>
2023-10-23 15:28:11 +0300

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,13 +8,12 @@
   */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
-	unsigned int j;
+	unsigned int i = 0;
 
-	for (i = 0 ; i < n ; i++)
+	while (i < n)
 	{
-		for (j = 0 ; j < n ; j++)
-		dest[i] = src[j];
+		dest[i] = src[i];
+		i++;
 	}
 	return (dest);
 }
