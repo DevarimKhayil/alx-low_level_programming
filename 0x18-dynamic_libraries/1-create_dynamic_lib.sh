00fff1cc68633424be3a3800e43cdbd36382e539
devarim <wachirabriangeita@gmail.com>
2024-01-15 12:45:02 +0300

    changes

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/bash
-gcc -c *.c -fpic && gcc *o -shared -o liball.so
+#gcc -c *.c -fpic && gcc *o -shared -o liball.so
+gcc -Wall -Werror -Wextra -pedantic -fPIC -c *.c
+gcc -shared -Wl,-soname,liball.so -o liball.so *.o
